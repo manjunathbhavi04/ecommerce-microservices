4ce6c8347315985c63c4b8647a3d9b2a0f0cb16b
manjunathbhavi04 <manjunathbhavi890@gmail.com>
2025-05-31 20:44:03 +0530

    Added findByEmail and existsByEmail queries to the userrepository

--- user-service/src/main/java/com/bhavi/ecommerce/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/bhavi/ecommerce/userservice/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.bhavi.ecommerce.userservice.repository;
 
-public class UserRepository extends {
+import com.bhavi.ecommerce.userservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
 }
