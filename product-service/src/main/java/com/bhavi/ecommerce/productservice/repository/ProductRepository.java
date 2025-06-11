1e6142162604d5f2ff1f39a9e3b2a7e1a17510c3
manjunathbhavi04 <manjunathbhavi890@gmail.com>
2025-06-11 23:48:35 +0530

    product repo added

--- product-service/src/main/java/com/bhavi/ecommerce/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/com/bhavi/ecommerce/productservice/repository/ProductRepository.java
@@ -4,7 +4,16 @@ import com.bhavi.ecommerce.productservice.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
-@Repository
+@Repository // Marks this interface as a Spring Data JPA repository component
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findByName(String name);
+
+    List<Product> findByCategory(String category);
+
+    List<Product> findByPriceBetween(BigDecimal minPrice, BigDecimal maxPrice);
 }
