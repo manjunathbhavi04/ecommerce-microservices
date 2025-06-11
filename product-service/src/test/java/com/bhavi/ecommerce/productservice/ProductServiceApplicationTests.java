eb51a54d21d38db83678c45ea4daed7b329d02c2
manjunathbhavi04 <manjunathbhavi890@gmail.com>
2025-06-11 21:00:34 +0530

    setting up with product service done with pom.xml

---
+++ product-service/src/test/java/com/bhavi/ecommerce/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhavi.ecommerce.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
